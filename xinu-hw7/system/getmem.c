43f581505e74b0d847cd84426d63db6075441bef
Casey O <lionspride4@gmail.com>
2015-03-24 17:34:46 -0500

    Added the finding first-fit portion to getmem.c

--- xinu-hw7/system/getmem.c
+++ xinu-hw7/system/getmem.c
@@ -21,5 +21,19 @@ void *getmem(ulong nbytes)
 	//       to new memory region, and add any remaining chunk
 	//       back into the free list.
 
+	// Search free list for first chunk large enough to fit.
+	memblk memBlock = freelist.next;
+	int foundBlock = FALSE;
+	while(memBlock != NULL && !foundBlock)
+	{
+		if(memBlock >= nbytes)
+			foundBlock = TRUE;
+		else
+			memBlock = memBlock.next;
+	}
+
+	// Break off region of requested size; return pointer to new mem region, and add any remianing chunk back into the free list.
+	
+
 	return (void *)SYSERR;
 }
